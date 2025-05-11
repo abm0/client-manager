377b1121eccf3de3149343b9b123f7e8f2dcebdf
Igor Dranichnikov <i.dranichnikov@tinkoff.ru>
2025-05-11 21:46:55 +0300

    добавлены конфигурационные файлы

--- backend/client_manager/settings.py
+++ backend/client_manager/settings.py
@@ -1,12 +1,78 @@
+from pathlib import Path
+
+# Базовая директория проекта
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Безопасность
+SECRET_KEY = 'django-insecure-change-this-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# Установленные приложения
 INSTALLED_APPS = [
-    ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'api',
 ]
 
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# URL конфигурация
+ROOT_URLCONF = 'client_manager.urls'
+
+# Шаблоны
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI
+WSGI_APPLICATION = 'client_manager.wsgi.application'
+
+# База данных (SQLite)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Валидация паролей (можно оставить пустым в dev)
+AUTH_PASSWORD_VALIDATORS = []
+
+# Локализация
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Статика
+STATIC_URL = '/static/'
+
+# Авто поле по умолчанию
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
