c78ae84946d94d07cba00e2a7d21f4443ad3425b
Mr Hackerman <someuser@gmail.com>
2025-06-05 15:45:02 +0300

    добавлена сумма сделок для клиентов

--- backend/client_manager/serializers.py
+++ backend/client_manager/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Client, Note, Transaction, ClientStatus, TransactionStatus
+from django.db.models import Sum
 
 class ClientStatusSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,13 +13,20 @@ class TransactionStatusSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ClientSerializer(serializers.ModelSerializer):
+    sum = serializers.SerializerMethodField()
+    
     class Meta:
         model = Client
         fields = '__all__'
         
+    def get_sum(self, obj):
+        return obj.transaction_set.filter(status=2).aggregate(total=Sum('value'))['total'] or 0
+        
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['full_name'] = f"{instance.first_name} {instance.patronymic or ''} {instance.last_name}"
+
+        
         return representation
 
 class NoteSerializer(serializers.ModelSerializer):
